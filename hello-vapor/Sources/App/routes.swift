09b1fe952c3918776191af916eb4d0a657cb20c3
vision8tech <vision8r@gmail.com>
2018-09-15 15:42:42 +0300

    sample rq-rs incl JSON

--- hello-vapor/Sources/App/routes.swift
+++ hello-vapor/Sources/App/routes.swift
@@ -3,10 +3,33 @@ import Vapor
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     
-    // Basic "Hello, world!" example
     router.get("hello") { req in
-        return "Hello, world!"
+        return "Hello, world!!!"
     }
+    
+    router.get("hello", "vapor") { req -> String in
+        return "Hello Vapor!"
+    }
+    
+    router.get("hello", String.parameter) { req -> String in
+        let name = try req.parameters.next(String.self)
+        return "Hello, \(name)!"
+    }
+    
+    // A JSON request-response example with POST
+    
+    struct InfoData: Content {
+        let name: String
+    }
+    
+    struct InfoResponse: Content {
+        let request: InfoData
+    }
+    
+    router.post(InfoData.self, at: "info") { req, data -> InfoResponse in
+        return InfoResponse(request: data)
+    }
+    
 
     // Example of configuring a controller
     let todoController = TodoController()
